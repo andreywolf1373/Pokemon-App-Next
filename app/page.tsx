29924e21cd70bca180d4fc0b18a4ec7d3c2f21a4
Andriy <andreywolf1373@gmail.com>
2024-04-06 18:38:12 +0300

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import { PokemonGrid } from "@/components/pokemon-grid";
-import Image from "next/image";
+
 import { getPokemonList } from "@/lib/pokemonAPI";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "@/components/ui/Loader";
 
 export default function Home() {
-  const {
-    data: pokemonList,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data: pokemonList, isLoading } = useQuery({
     queryKey: ["pokemonList"],
     queryFn: getPokemonList,
   });
