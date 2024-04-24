64d66d6752204899e4c1de833bff4c268055fc15
Andriy <andreywolf1373@gmail.com>
2024-04-24 17:24:49 +0300

    added pagination

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,41 @@
 import { PokemonGrid } from "@/components/pokemon-grid";
 
 import { getPokemonList } from "@/lib/pokemonAPI";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import Loader from "@/components/ui/Loader";
+import { useState } from "react";
 
 export default function Home() {
+  const [page, setPage] = useState(0);
   const { data: pokemonList, isLoading } = useQuery({
-    queryKey: ["pokemonList"],
-    queryFn: getPokemonList,
+    queryKey: ["pokemonList", page, { keepPreviousData: true }],
+    queryFn: () => getPokemonList(page),
   });
 
   if (isLoading) return <Loader />;
 
-  return <PokemonGrid pokemonList={pokemonList} isLoading={isLoading} />;
+  return (
+    <>
+      <PokemonGrid pokemonList={pokemonList} isLoading={isLoading} />
+      <div className="flex gap-8">
+        <button
+          className="group rounded-lg border border-white m-3 px-5 py-1"
+          onClick={() => setPage(page - 30)}
+          disabled={!page}
+        >
+          Prev
+        </button>
+        <button
+          className="group rounded-lg border border-white m-3 px-5 py-1"
+          onClick={() => setPage(page + 30)}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
 }
 
-// add fix width
-// add pagination
+// add fix width V
+// add pagination V
 // debouns search watch info or add submit
